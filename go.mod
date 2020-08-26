9ce9e4931bd1971605bff629e7139f238acb980e
mind1949 <lianjie1949@gmail.com>
2020-08-27 03:45:19 +0800

    chore: go mod tidy

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/mind1949/googletrans
 
 go 1.14
-
-require golang.org/x/text v0.3.3
--- googletrans_bench_test.go
+++ googletrans_bench_test.go
@@ -2,15 +2,13 @@ package googletrans
 
 import (
 	"testing"
-
-	"golang.org/x/text/language"
 )
 
 func BenchmarkGet(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		params := TranslateParams{
 			Src:  "auto",
-			Dest: language.SimplifiedChinese.String(),
+			Dest: "zh-CN",
 			Text: "Go is an open source programming language that makes it easy to build simple, reliable, and efficient software. ",
 		}
 		Translate(params)
